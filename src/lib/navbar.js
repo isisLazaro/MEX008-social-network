2bf8e8536cd86939e71b5ec21a23cdc5910743f8
Isis Lázaro <isislazaro@gmail.com>
2019-08-08 09:11:44 -0500

    agrupando links en navbar

--- src/lib/navbar.js
+++ src/lib/navbar.js
@@ -3,8 +3,10 @@ let Navbar = {
         let view = /*html*/`
             <nav id = "navbar">
                 <a href = "./#/"><img src = "imgs/logo-rojo.png" id = "logo"></a>
-                <a href = "./#/register" class = "btn-start"> Crea tu cuenta</a>
-                <a href = "./#/login" class = "btn-start">Inicia sesión</a>
+                <nav id = "navbar-cuenta">
+                    <a href = "./#/register" class = "btn-start"> Crea tu cuenta</a>
+                    <a href = "./#/login" class = "btn-start">Inicia sesión</a>
+                </nav>
             </nav>
         `
         return view
