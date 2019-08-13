9f145f50caf658a43e4e028d54068d62dc6ce52d
Isis Lázaro <isislazaro@gmail.com>
2019-08-13 16:37:28 -0500

    nueva barra de navegación

--- src/lib/navbar.js
+++ src/lib/navbar.js
@@ -1,24 +1,21 @@
 let Navbar = {    
     render : async () => { 
         let view = /*html*/`
-     <nav>
-      <nav id = "navbar-dsk" class = "navbar">
-      <a href = "./#/"><img src = "img/Logo-white.png" id = "logo"></a><br>
-       <ul id="nav-mobile" class="right ">
-         <li class="size-li"><a href = "./#/login" id = "btn-login" class = "linksToJoin" >Inicia sesión</a></li>
-         <li class="size-li"><a href = "./#/register" id = btn-register class = "linksToJoin"> Crea tu cuenta</a>
-         </li>
-       </ul>
-      </nav>
-     <!--  mobile version -->
-     <nav id = "navbar-mbl" class = "navbar">
-     <ul id="nav-mobile" class="right">
-      <li>
-      <a href = "./#/"><img src = "img/Logo-white.png" id = "logo"></a>
-      </li>
-     </ul>
-     </nav>
-     </nav>
+          <nav>
+            <nav id = "navbar-dsk" class = "navbar">
+              <a href = "./#/"><img src = "img/Logo-white.png" id = "logo"></a><br>
+              <ul id="nav-mobile" class="right ">
+                <li class="size-li"><a href = "./#/login" id = "btn-login" class = "linksToJoin" >Inicia sesión</a></li>
+                <li class="size-li"><a href = "./#/register" id = btn-register class = "linksToJoin"> Crea tu cuenta</a></li>
+              </ul>
+            </nav>
+            <!--  mobile version -->
+            <nav id = "navbar-mbl" class = "navbar">
+              <ul id="nav-mobile" class="right">
+                <li><a href = "./#/"><img src = "img/Logo-white.png" id = "logo"></a></li>
+              </ul>
+            </nav>
+          </nav>
         `
         return view
     }
