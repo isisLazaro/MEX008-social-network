398e4b3d29bd132ab03d5e863c458e9cd2291763
Rosa Isela Lopez Palma <rosaiselalopezpalma@MacBook-Air-de-Rosa.local>
2019-08-14 16:41:14 -0500

    Depurando código

--- src/lib/login.js
+++ src/lib/login.js
@@ -40,9 +40,8 @@ let Login = {
         ui = new firebaseui.auth.AuthUI(firebase.auth());
         }
         ui.start('#firebaseui-auth-container', uiConfig);
-
-
     }
+
 }
 
 export default Login;
