8a9a381a96ebecf66ac23c42e1215871ee216a26
Rosa Isela Lopez Palma <rosaiselalopezpalma@192.168.1.2>
2019-08-17 15:50:48 -0500

    agregando funciones para guardar post en firebase, mostrarlas en pantalla, borrarlas y editarlas

--- src/lib/inicio.js
+++ src/lib/inicio.js
@@ -41,6 +41,7 @@ let Inicio = {
                     </div>
                 </div>
             </div>
+
             <div class="row">
                 <div class="col s12 m7">
                     <div class="card">
@@ -60,10 +61,110 @@ let Inicio = {
                 </div>
             </div>
         </div>
+
+
+        <div class="container">
+        <input type="text" placeholder="¿Que está pasando?" id="nombre" class="form-control my-3">
+        <button class="btn btn-info btn-save" id="boton">Guardar</button>
+        <button class="btn btn-info" id="editar">Editar</button>
+
+        <table class="table my-3">
+            <thead>
+            <tr>
+                <th scope="col">Tus publicaciones</th>
+            </tr>
+            </thead>
+            <tbody id="tabla">
+            </tbody>
+        </table>
+       </div>
         `
         return view
     }
     , after_render: async () => {
+            // **************** PUBLICANDO POST *****************  
+  //Creando una variable para firebase
+  const db = firebase.firestore();
+  //-------------------------------------------------------------------
+  const btnSavePost = document.getElementsByClassName("btn-save")[0];
+  
+  // **************** GUARDANDO ELEMENTOS EN FIRESTORE *****************
+  
+  
+  const guardar = () => {
+    const nombre = document.getElementById("nombre").value;
+  
+    db.collection("users").add({
+      first: nombre,
+    })
+    .then((docRef) => {
+      console.log("Document written with ID: ", docRef.id);
+      document.getElementById("nombre").value = "";
+    })
+    .catch((error) => {
+      console.error("Error adding document: ", error);
+    });
+  };
+  
+  btnSavePost.addEventListener("click", guardar);
+  
+  
+  // **************** MOSTRANDO ARCHIVOS *****************
+  const tabla = document.getElementById("tabla");
+  tabla.innerHTML = "";
+  //onSnapshot es una agente de cambio, va a estar vigilando y ayuda a que los cambios se muestren en tiempo real
+  db.collection("users").onSnapshot((querySnapshot) => {
+    tabla.innerHTML = "";
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data().first}`);
+        tabla.innerHTML  += `
+                <tr>
+                  <td class="my-3">${doc.data().first}</td>
+                  <td class="my-6"><button class="btn btn-danger" onclick="eliminar('${doc.id}')" id="btnEliminar">Eliminar</button></td>
+                  <td class="my-6"><button class="btn btn-warning" onclick="editar('${doc.id}', '${doc.data().first}')">Editar</button></td>
+                </tr>
+        `
+    });
+  });
+  
+  // **************** BORRANDO ELEMENTOS *****************
+  const eliminar = (id) => {
+    db.collection("users").doc(id).delete().then(() => {
+      console.log("Document successfully deleted!");
+    }).catch((error) => {
+      console.error("Error removing document: ", error);
+    });
+  };
+  
+  
+  // **************** EDITANDO POST *****************
+  const editar = (id, nombre,) => {
+    document.getElementById("nombre").value = nombre;
+  
+    const botonEditar = document.getElementById("editar");
+  //wasingtonRef se utiliza para hacer un update
+    botonEditar.onclick = () => {
+      const washingtonRef = db.collection("users").doc(id);
+      // Set the "users" field of  id
+      const nombre = document.getElementById("nombre").value;
+  
+        return washingtonRef.update({
+          first: nombre,
+  
+        })
+        .then(() => {
+            console.log("Document successfully updated!");
+            boton.innerHTML = "Guardar";
+            document.getElementById("nombre").value = "";
+        })
+        .catch((error) => {
+            // The document probably doesn't exist.
+            console.error("Error updating document: ", error);
+        });
+    }
+    };
+
+
         const btnLogOut = document.getElementById("btnLogOut");
 
         const logOut = () => firebase.auth().signOut();
@@ -84,5 +185,7 @@ let Inicio = {
 
         btnLogOut.addEventListener('click', logOutOnClick);
     }
+
+    
 }
 export default Inicio;
